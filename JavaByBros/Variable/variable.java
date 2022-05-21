05075bd656091f3d9751bd8c9f0d70f75ec8e462
KaptainCS3 <mbiapplegryn@gmail.com>
2022-05-21 01:25:04 +0100

    primitive data types in java

---
+++ JavaByBros/Variable/variable.java
@@ -0,0 +1,13 @@
+public class variable {
+    public static void main(String[] args){
+     //Variables in JAVA
+        int age = 21;   //initializing a int variable
+        long PlanetAge = 10958045830458304L;
+        float PI = 3.14F;
+        double Weight = 58.4854973;
+        boolean isAdmin = false;
+        char email = '@';
+        String name = "KaptainCS3";
+        System.out.println(email+name+" is my github name check it out");
+    }
+}
