433455747345e3f68e78a7915cd08ddd3289fb76
KaptainCS3 <mbiapplegryn@gmail.com>
2022-05-22 20:29:10 +0100

    concept done in javascript too

---
+++ JavaByBros/forEachLoop/forEach.java
@@ -0,0 +1,11 @@
+package forEachLoop;
+public class forEach {
+    public static void main(String[] arg){
+        int num[] = {2, 4, 6, 8, 10 ,12};
+        System.out.print("\n");
+        for(int nums : num){
+            System.out.println(nums);
+        }
+        System.out.print("\n");
+    }
+}
